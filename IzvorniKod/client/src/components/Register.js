413e341ac177fc976d9ee3b288f34613e13403b3
ivanbilobrk <ivan.bilobrk@fer.hr>
2022-10-29 17:17:12 +0200

    sign-up fix

--- IzvorniKod/client/src/components/Register.js
+++ IzvorniKod/client/src/components/Register.js
@@ -1,7 +1,6 @@
 import { useRef, useState, useEffect } from "react";
 import {faCheck, faTimes, faInfoCircle} from "@fortawesome/free-solid-svg-icons"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import SignUpMUI from './SignUpMUI'
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
