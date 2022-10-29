62ae5b230869d937eb06ffaba8275c4c79334d9a
ivanbilobrk <ivan.bilobrk@fer.hr>
2022-10-29 17:10:31 +0200

    basic-signup

---
+++ IzvorniKod/client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Register} from './components/Register'
+import React, {Component} from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <Register/>
+    </div>
+  );
+}
+
+export default App;
---
+++ IzvorniKod/client/src/components/Register.js
@@ -0,0 +1,311 @@
+import { useRef, useState, useEffect } from "react";
+import {faCheck, faTimes, faInfoCircle} from "@fortawesome/free-solid-svg-icons"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import SignUpMUI from './SignUpMUI'
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { counter } from "@fortawesome/fontawesome-svg-core";
+
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const REGISTER_URL = '/register';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Sahisti
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function Register() {
+
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [name, setName] = useState('');
+    const [validName, setValidName] = useState(false);
+    const [nameFocus, setNameFocus] = useState(false);
+
+    const [surname, setsurname] = useState('');
+    const [validsurname, setvalidsurname] = useState(false);
+    const [surnameFocus, setsurnameFocus] = useState(false);
+
+
+    const [user, setUser] = useState('');
+    const [validUser, setValidUser] = useState(false);
+    const [userFocus, setUserFocus] = useState(false);
+
+    const[email, setEmail] = useState('');
+    const[validEmail, setValidEmail] = useState(false);
+    const[emailFocus, setEmailFocus] = useState(false);
+
+    const [pwd, setPwd] = useState('');
+    const [validPwd, setValidPwd] = useState(false);
+    const [pwdFocus, setPwdFocus] = useState(false);
+
+    const [matchPwd, setMatchPwd] = useState('');
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus, setMatchFocus] = useState(false);
+
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    const[nameCount, setNameCount] = useState(false);
+    const[surnameCount, setsurNameCount] = useState(false);
+    const[usernameCount, setuserNameCount] = useState(false);
+    const[emailCount, setemailCount] = useState(false);
+    const[passwordCount, setpasswordCount] = useState(false);
+    const[matchCount, setmatchCount] = useState(false);
+
+    useEffect(() => {
+        setValidUser(USER_REGEX.test(user));
+    }, [user])
+
+    useEffect(()=>{
+        setValidName(name?.length !== 0)
+    },[name])
+
+    useEffect(()=>{
+        setvalidsurname(surname?.length !== 0)
+    },[surname])
+
+    useEffect(()=>{
+        setValidEmail(EMAIL_REGEX.test(email))
+    },[email])
+
+    useEffect(() => {
+        setValidPwd(PWD_REGEX.test(pwd));
+        setValidMatch(pwd === matchPwd);
+    }, [pwd, matchPwd])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pwd, matchPwd])
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    let email = data.get('email');
+    
+  };
+
+  const ErrorInput = (count, validation, focus)=>{
+    if(count === false)
+        return false;
+
+        if(!focus){
+            return !validation;
+        }
+    return false;
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Registracija
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  name="firstName"
+                  required
+                  onChange={(e)=>{
+                    setName(e.target.value)
+                    setNameCount(true);
+                  }}
+                  onFocus = {() =>{
+                    setNameFocus(true);
+                  }}
+                  onBlur = {()=>{
+                    setNameFocus(false);
+                  }}
+                  fullWidth
+                  id="firstName"
+                  label="Ime"
+                  autoFocus
+                  error={ErrorInput(nameCount, validName, nameFocus)}
+                  helperText = {ErrorInput(nameCount, validName, nameFocus)? 'Ime ne smije biti prazno':''}
+                />
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Prezime"
+                  name="lastName"
+                  onChange={(e)=>{
+                    setsurname(e.target.value)
+                    setsurNameCount(true);
+                  }}
+                  onFocus = {() =>{
+                    setsurnameFocus(true);
+                    
+                  }}
+                  onBlur = {()=>{
+                    setsurnameFocus(false);
+                  }}
+                  error={ErrorInput(surnameCount, validsurname, surnameFocus)}
+                  helperText = {ErrorInput(surnameCount, validsurname, surnameFocus)? 'Prezime ne smije biti prazno':''}
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                 inputProps={{ maxLength: 24 }}
+                  required
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  name="username"
+                  onChange={(e)=>{
+                    setUser(e.target.value)
+                    setuserNameCount(true);
+                  }}
+                  onFocus = {() =>{
+                    setUserFocus(true);
+                  }}
+                  onBlur = {()=>{
+                    setUserFocus(false);
+                  }}
+                  error={ErrorInput(usernameCount, validUser, userFocus)}
+                  helperText = {ErrorInput(usernameCount, validUser, userFocus) ?'Username mora biti između 4-24 znaka te'
+                  +' započinje velikim ili malim slovom nakon čega slijedi:slova, -, _, brojke':''}
+                  
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  onChange={(e)=>{
+                    setEmail(e.target.value)
+                    setemailCount(true);
+                  }}
+                  onFocus = {() =>{
+                    setEmailFocus(true);
+                  }}
+                  onBlur = {()=>{
+                    setEmailFocus(false);
+                  }}
+                  error={ErrorInput(emailCount, validEmail, emailFocus)}
+                  helperText = {ErrorInput(emailCount, validEmail, emailFocus) ?'Unesite ispravan e-mail.':''}
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                   inputProps={{ maxLength: 24 }}
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  onChange={(e)=>{
+                    setPwd(e.target.value)
+                    setpasswordCount(true);
+                  }}
+                  onFocus = {() =>{
+                    setPwdFocus(true);
+                  }}
+                  onBlur = {()=>{
+                    setPwdFocus(false);
+                  }}
+                  error={ErrorInput(passwordCount, validPwd, pwdFocus)}
+                  helperText = {ErrorInput(passwordCount, validPwd, pwdFocus) ?'Lozinka mora biti između 8 i 24 znaka te se sastoji od: '
+                  +'barem jednom malog slova, barem jednog velikog slova, barem jedne znamenke te barem jednog specijalnog znaka.':''}
+
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  inputProps={{ maxLength: 24 }}
+                  required
+                  fullWidth
+                  name="matchpassword"
+                  label="Confirm password"
+                  type="password"
+                  id="matchpassword"
+                  onChange={(e)=>{
+                    setMatchPwd(e.target.value)
+                    setmatchCount(true);
+                  }}
+                  onFocus = {() =>{
+                    setMatchFocus(true);
+                  }}
+                  onBlur = {()=>{
+                    setMatchFocus(false);
+                  }}
+                  error={ErrorInput(matchCount, validMatch, matchFocus)}
+                  helperText = {ErrorInput(matchCount, validMatch, matchFocus)?'Lozinke se ne podudaraju':''}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Registriraj se!
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Već imaš profil? Prijavi se!
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export {Register}
