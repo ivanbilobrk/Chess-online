22bc4632ed76202ca3723608a8106ac522b465ff
ivanbilobrk <ivan.bilobrk@fer.hr>
2022-11-03 15:40:34 +0100

    fix env with database

--- IzvorniKod/API/db/index.js
+++ IzvorniKod/API/db/index.js
@@ -1,10 +1,10 @@
 const {Pool} = require('pg');
 const dotenv = require('dotenv');
-dotenv.config({ path: '../.env' });
+dotenv.config();
 const env = process.env;
 
 const pool = new Pool({
-    user: env.DB_NAME,
+    user: env.DB_USER,
     host: env.DB_HOST,
     database: env.DB_NAME,
     password: env.DB_PASSWORD,
