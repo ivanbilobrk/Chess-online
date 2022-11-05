b38c8fc1e67680537ab275d73338deb4de3bff13
ilanvezmarovic <iv53140@fer.hr>
2022-11-05 22:10:48 +0100

    userController - fetching user info from database

--- IzvorniKod/API/routes/user.routes.js
+++ IzvorniKod/API/routes/user.routes.js
@@ -1,12 +1,7 @@
 const express = require('express');
-const { StatusCodes } = require('http-status-codes');
 const router = express.Router();
+const userController = require('../controllers/userController');
 
-
-router.get('/:user', (req, res) =>{
-    //trazimo podatke za usera
-    return res.status(StatusCodes.OK).json({podatci:["prvi", "drugi"]})
-
-});
+router.get('/:user', userController.getUserInfo );
 
 module.exports = router;
