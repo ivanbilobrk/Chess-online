cbc708230f68ee7b3d3897cddcfb899ca143e5c2
unknown <ab53173@fer.hr>
2022-11-09 14:01:22 +0100

    a

---
+++ IzvorniKod/API/db/index.js
@@ -0,0 +1,24 @@
+const {Pool} = require('pg');
+require('dotenv').config();
+const env = process.env;
+
+const pool = new Pool({
+    user: env.DB_USER,
+    host: env.DB_HOST,
+    database: env.DB_NAME,
+    password: env.DB_PASSWORD,
+    port: parseInt(env.DB_PORT),
+});
+
+module.exports = {
+    query: (text, params) => {
+        const start = Date.now();
+        return pool.query(text, params)
+            .then(res => {
+                const duration = Date.now() - start;
+                console.log('executed query', {text, rows: res.rows});
+                return res;
+            });
+    },
+    pool: pool
+}
---
+++ IzvorniKod/API/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const verifyJWT = require('./middleware/verifyJWT');
+var cors = require('cors')
+
+
+app.use(cors({credentials: true, origin: true}));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+app.get('/', (req, res)=> {
+    res.send("home")
+})
+
+app.use('/register', require('./routes/register.routes'));
+app.use('/login', require('./routes/login.routes'));
+app.use('/logout', require('./routes/logout.routes'));
+app.use('/refresh', require('./routes/refresh.routes'));
+
+app.use(verifyJWT);  //every route after this line here will use verifyJWT middleware
+
+//tester - tester route that will demonstrate use of tokens
+//download thunder client in vsc, and follow these steps for testing
+// 1) register a new user at /register (send the parameters through body of POST request)
+// 2) login with that username and password at /login (send the username and pwd throught body)
+// 3) you will get AccessToken in respone, copy it
+// 4) send the GET request to the /tester with copied AccessToken in Auth->Bearer 
+// 5) you will be able to access the /tester page until the token expires
+// 6) when AccessToken expires, send GET request to the /refresh route and you will get a new AccessToken
+// 7) send the GET request to the /logout route, RefreshToken will be deleted from the database
+// 8) now if you try to GET /refresh you will get 401 Unauthorized because you aren't logged in anymore
+
+app.use('/user', require('./routes/user.routes'));
+
+app.use((req, res)=>{
+    res.status(404).json({error: 'Not found'});
+})
+
+app.use((error, req, res, next)=>{
+    res.status(error.status).json({
+        message: error.message ? error.message : 'Dogodila se greška.',
+        stack: error.showStack ? error.showStack : {},
+    });
+    next();
+});
+
+app.listen(parseInt(process.env.PORT));
