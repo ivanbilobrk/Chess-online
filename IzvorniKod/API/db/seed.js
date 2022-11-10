0bea1a19d501f39ec0c617e4c04a56fb1d415f03
lara m <lm53176@fer.hr>
2022-11-10 16:54:27 +0100

    dodan sql

--- IzvorniKod/API/db/seed.js
+++ IzvorniKod/API/db/seed.js
@@ -11,35 +11,155 @@ const pool = new Pool({
 });
 
 
+
 const sql_create_users = `CREATE TABLE users (
     id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
     name text NOT NULL,
     surname text NOT NULL,
     username text NOT NULL UNIQUE,
     email text NOT NULL UNIQUE,
-    pwdHash  NOT NULL,
+    pwdHash text NOT NULL,
     role text NOT NULL,
     refreshToken text
 )`;
 
 const sql_create_users_id_index = `CREATE  UNIQUE INDEX idx_usersId ON users(id)`;
 
+
+
+const sql_create_training = `CREATE TABLE training (
+  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+  trainer_id int REFERENCES users(id),
+  trainingStartTimeDate timestamp NOT NULL,
+  trainingDurationMin int NOT NULL
+)`;
+
+const sql_create_training_id_index =`CREATE  UNIQUE INDEX idx_trainingId ON training(id)`;
+
+const sql_create_tournament =`CREATE TABLE tournament(
+  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+  trainer_id int REFERENCES users(id),
+  tournamentStartTimeDate timestamp NOT NULL,
+  tournamentDurationMin int NOT NULL,
+  participantsNo int 
+)`;
+
+const sql_create_tournament_id_index =`CREATE  UNIQUE INDEX idx_tournamentId ON tournament(id)`;
+
+const sql_create_scheduledTournament =`CREATE TABLE scheduledTournament(
+user_id int REFERENCES users(id),
+tournament_id int REFERENCES tournament(id),
+PRIMARY KEY (user_id, tournament_id)
+
+)`;
+
+const sql_create_scheduledTraining =`CREATE TABLE scheduledTraining(
+	user_id int REFERENCES users(id),
+    training_id int REFERENCES training(id),
+    PRIMARY KEY(user_id, training_id)
+
+
+)`;
+
+
+const sql_create_news = `CREATE TABLE news(
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+    trainer_id int REFERENCES users(id),
+    content text NOT NULL
+)`;
+
+const sql_create_news_id_index =`CREATE  UNIQUE INDEX idx_newsId ON news(id)`;
+
+
+const sql_create_dailyTactics= `CREATE TABLE dailyTactics(
+  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+  trainer_id int REFERENCES users(id),
+  content text NOT NULL,
+  solution text NOT NULL
+)`;
+
+const sql_create_dailyTactics_id_index =`CREATE  UNIQUE INDEX idx_dailyTacticsId ON dailyTactics(id)`;
+
+
+const sql_create_membership =`CREATE TABLE membership(
+  user_id int REFERENCES users(id),
+  periodStart int NOT NULL,
+  periodEnd int NOT NULL,
+  isPaid boolean NOT NULL,
+  PRIMARY KEY(user_id)
+)`;
+
+
+const sql_create_reportedMistake =`CREATE TABLE reportedMistake(
+	user_id int REFERENCES users(id),
+	tactic_id int REFERENCES dailyTactics(id),
+	trainer_id int REFERENCES users(id),
+	preposedMove text NOT NULL,
+	moveDescription text NOT NULL,
+	isFixed boolean NOT NULL,
+	PRIMARY KEY(user_id, tactic_id)
+
+)`;
+
+
+
+const sql_create_score = `CREATE TABLE score(
+	user_id int REFERENCES users(id),
+	tactic_id int REFERENCES dailyTactics(id),
+	solvingTime time NOT NULL,
+	accuracy float NOT NULL,
+	PRIMARY KEY(user_id, tactic_id)
+	
+)`;
+
+
+
+
+
+
+
+
+
+
+
 //const sql_insert_users = `INSERT INTO users (username, name, surname, email, pwdhash, role) VALUES ('admin', 'Adminko', 'Administratović', 'null@admin', 'adminpass', 'admin')`
 
 let table_names = [
-    "users"
+    "users",
+    "training",
+    "tournament",
+    "scheduledTournament",
+    "scheduledTraining",
+    "news",
+    "dailyTactics",
+    "membership",
+    "reportedMistake",
+    "score"
 ]
 
 let tables = [
-    sql_create_users
+    sql_create_users,
+    sql_create_training,
+    sql_create_tournament,
+    sql_create_scheduledTournament,
+    sql_create_scheduledTraining,
+    sql_create_news,
+    sql_create_dailyTactics,
+    sql_create_membership,
+    sql_create_reportedMistake,
+    sql_create_score
 ];
 
 let table_data = [
-    //sql_insert_users
+   // sql_insert_users
 ]
 
 let indexes = [
-    sql_create_users_id_index
+    sql_create_users_id_index,
+    sql_create_tournament_id_index,
+    sql_create_training_id_index,
+    sql_create_news_id_index,
+    sql_create_dailyTactics_id_index
 ];
 
 
