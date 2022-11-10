ca129e1367801b98a87bf673bbe592a7de1bfae3
AnteoV <tela.vukasovic@gmail.com>
2022-11-10 23:44:52 +0100

    About page, header skoro zgotovljen

--- IzvorniKod/client/src/components/Header.js
+++ IzvorniKod/client/src/components/Header.js
@@ -1,16 +1,42 @@
 import * as React from 'react';
-import { Link } from "react-router-dom";
-import "./Header.css"
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+import ToolbarGroup from '@mui/material/Toolbar';
 
 
-const Header = () =>{
-    return <header className="header">
-        <div className="container">
-           <Link className="child linksH" to="/login">Prijava u sustav</Link>
-           <Link className="child linksH title" to="/">ŠAHISTI</Link>
-           <Link className="child linksH" to="/profile">Osobni podaci</Link>
-        </div>
-    </header>
+export default function Header() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar sx ={{justifyContent: "space-between"}}>
+            <ToolbarGroup >
+                <Link to="/" style={{textDecoration: 'none', color: 'white'}} >
+                    <Typography variant="h6" component="div" style={{padding: '1px 10px', fontSize: '30px'}} sx={{ 
+                    ':hover': {
+                    bgcolor: '#1565c0', // theme.palette.primary.main
+                    color: 'white',
+                     }, }}>
+                    Šahisti
+                    </Typography>
+                </Link>
+            </ToolbarGroup>
+            <ToolbarGroup>
+                <Link to="/profile" style={{textDecoration: 'none', color: 'white', float: 'right'}}> 
+                    <Button color="inherit" sx={{ mr:1 }}>Osobni podatci</Button>
+                </Link>
+                <Link to="/login" style={{textDecoration: 'none', color: 'white', float: 'right'}}> 
+                    <Button color="inherit" style={{color: '#faebd7'}} sx={{ mr:1 }}>Prijava u sustav</Button>
+                </Link>
+                <Link to="/register" style={{textDecoration: 'none', color: 'white', float: 'right'}}> 
+                    <Button color="inherit" style={{color: '#faebd7'}}>Registracija u sustav</Button>
+                </Link>
+            </ToolbarGroup>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
-
-export default Header;
--- IzvorniKod/client/src/pages/AboutPage.js
+++ IzvorniKod/client/src/pages/AboutPage.js
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import AboutTable from "../components/AboutTable";
+import AboutText from "../components/AboutText";
 
 export default function AboutPage(){
     return(
         <>
         <Header></Header>
-        <div>Ovo je about</div>
+        <h1>O projektu</h1>
+        <AboutText></AboutText>
+        <AboutTable></AboutTable>
         <Footer></Footer>
         </>
     );
